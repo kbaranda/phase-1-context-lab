cd779cd4fa09831f7bae002bb6f0db33409228ef
Kat <bolivarkat@yahoo.com>
2021-10-10 19:37:42 -0500

    Completed assignmnet

--- index.js
+++ index.js
@@ -1,4 +1,76 @@
 /* Your Code Here */
+const allEmployees = [];
+
+function createEmployeeRecord([String1, String2, String3, Number]) {
+    const employeeRecord = {
+        firstName: String1,
+        familyName: String2,
+        title: String3,
+        payPerHour: Number,
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    allEmployees.push(employeeRecord)
+    return employeeRecord
+}
+
+function createEmployeeRecords(arrays) {
+  const arrOfObjs = []
+  arrays.forEach((array) => {
+    const newObj = createEmployeeRecord(array)
+    arrOfObjs.push(newObj)
+  })
+  return arrOfObjs
+}
+
+function createTimeInEvent(datestamp) {
+    const hour = datestamp.split(" ")[1]
+    const date = datestamp.split(" ")[0]
+    const timeInEmployee = {
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: date
+    }
+    const timeInEvents = this.timeInEvents
+    timeInEvents.push(timeInEmployee)
+    return this
+}
+
+function createTimeOutEvent(datestamp) {
+    const hour = datestamp.split(" ")[1]
+    const date = datestamp.split(" ")[0]
+    const timeOutEmployee = {
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date
+    }
+    const timeOutEvents = this.timeOutEvents
+    timeOutEvents.push(timeOutEmployee)
+    return this
+}
+
+function hoursWorkedOnDate(datestamp) {
+    let timeIn =""
+    let timeOut =""
+    this.timeInEvents.forEach((x) => {
+        if (x.date === datestamp) {
+            timeIn = x.hour
+        }
+    })
+    this.timeOutEvents.forEach((x) => {
+        if (x.date === datestamp) {
+            timeOut = x.hour
+        }
+    })
+    const hoursWorked = (timeOut - timeIn) / 100
+    return hoursWorked
+}
+
+function wagesEarnedOnDate(datestamp) {
+    const hoursWorked = hoursWorkedOnDate.call(this, datestamp)
+    const paycheck = hoursWorked * this.payPerHour
+    return paycheck
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +93,11 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName)
+}
+
+function calculatePayroll(array) {
+    const reducer = (previousValue, employee) => previousValue + allWagesFor.call(employee)
+    return array.reduce(reducer, 0)
+}
